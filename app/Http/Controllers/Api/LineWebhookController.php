c4d427a8459c05ae6ffb68eb5ff17682ad36734c
chien_rong chiu <yoyo70202@gmail.com>
2020-09-23 17:39:01 +0800

    same as above image

--- app/Http/Controllers/Api/LineWebhookController.php
+++ app/Http/Controllers/Api/LineWebhookController.php
@@ -9,9 +9,27 @@ use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\SignatureValidator;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
 use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
 use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+
+#確認型
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+
+#按鈕
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+
+#座標
+use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+
+#Line貼圖
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+
 use Exception;
 
 class LineWebhookController extends Controller
@@ -55,43 +73,43 @@ class LineWebhookController extends Controller
 					  $column = new CarouselColumnTemplateBuilder("標題".$i, "說明".$i, $img_url , $actions);
 					  $columns[] = $column;
 					}
-					$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
-					$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $carousel);
+					$carousel = new CarouselTemplateBuilder($columns);
+					$msg = new TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $carousel);
                 	$lineBot->replyMessage($replyToken,$msg);// 回復使用者輸入
                 }
                 if ($text == "粉絲獨享") {
                 	$actions = array(
-					  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("是", "ans=Y"),
-					  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("否", "ans=N")
+					  new PostbackTemplateActionBuilder("是", "ans=Y"),
+					  new PostbackTemplateActionBuilder("否", "ans=N")
 					);
-					$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder("問題", $actions);
-					$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
+					$button = new ConfirmTemplateBuilder("問題", $actions);
+					$msg = new TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
                 	$lineBot->replyMessage($replyToken,$msg);// 回復使用者輸入
                 }
                 if ($text == "常見問題") {
                 	$actions = array(
 					  //一般訊息型 action
-					  new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("按鈕1","文字1"),
+					  new MessageTemplateActionBuilder("按鈕1","文字1"),
 					  //網址型 action
-					  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Google","http://www.google.com"),
+					  new UriTemplateActionBuilder("Google","http://www.google.com"),
 					  //下列兩筆均為互動型action
-					  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("下一頁", "page=3"),
-					  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("上一頁", "page=1")
+					  new PostbackTemplateActionBuilder("下一頁", "page=3"),
+					  new PostbackTemplateActionBuilder("上一頁", "page=1")
 					);
 
 					$img_url = "https://www.sample-videos.com/img/Sample-jpg-image-50kb.jpg";
-					$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("按鈕文字","說明", $img_url, $actions);
-					$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
+					$button = new ButtonTemplateBuilder("按鈕文字","說明", $img_url, $actions);
+					$msg = new TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
 					$lineBot->replyMessage($replyToken,$msg);// 回復使用者輸入
                 }
                 if ($text == "熱銷必敗") {
-                	$msg = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("群義房屋", "台中市南屯區文心路一段424號", 24.1503955, 120.646975);
+                	$msg = new LocationMessageBuilder("群義房屋", "台中市南屯區文心路一段424號", 24.1503955, 120.646975);
                 	$lineBot->replyMessage($replyToken, $msg);// 回復使用者輸入
                 }
                 if ($text == "推薦好友") {
                 	$packageId = '1';
                 	$stickerId = '2';
-                	$msg = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($packageId,$stickerId);
+                	$msg = new StickerMessageBuilder($packageId,$stickerId);
                 	$lineBot->replyMessage($replyToken, $msg);// 回復使用者輸入
                 }
                 else
