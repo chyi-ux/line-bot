5a720f9848622807265e6f15f80ee40d17d35195
chien_rong chiu <yoyo70202@gmail.com>
2020-09-23 11:07:50 +0800

    TestController test page

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,34 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use App\Services\LineBotService;
-use App\Services\WebhookResponseService;
-use App\Transformers\Requests\WebhookRequestTransformer;
 
 class TestController extends Controller
 {
     //
-
-    public function __construct(WebhookResponseService $responseService,LineBotService $bot)
+    public function index()
     {
-        $this->bot = $bot;
-        $this->responseService = $responseService;
-    }
-
-    public function index(Request $request,WebhookRequestTransformer $reqTransformer)
-    {
-        Log::info("Request from Line",$request->all());
-
-        foreach ($request['events'] as $event) {
-
-            $webhookRequest = $reqTransformer->tramsforRequest($event);
-
-            $this->bot->pushMessage(
-                $webhookRequest['userId'],
-                $response = $this->responseService->returnResponse($webhookRequest['content']));
-        }
-
-        return $response;
+    	return view('test');
     }
 }
