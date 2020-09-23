4615c32659877228030611a9091a5b8a779d415f
chien_rong chiu <yoyo70202@gmail.com>
2020-09-23 15:35:08 +0800

    same as above modified video

--- app/Http/Controllers/Api/LineWebhookController.php
+++ app/Http/Controllers/Api/LineWebhookController.php
@@ -33,9 +33,9 @@ class LineWebhookController extends Controller
           
             $events = $lineBot->parseEventRequest($request->getContent(), $signature);
             $image = "{
-            	'type': 'image',
-		        'originalContentUrl': 'https://cdn.pixabay.com/photo/2020/09/18/14/13/mountains-5581991__340.jpg',
-		        'previewImageUrl': 'https://cdn.pixabay.com/photo/2020/09/18/14/13/mountains-5581991__340.jpg'
+            	'type': 'video',
+		        'originalContentUrl': 'https://www.sample-videos.com/video123/mp4/240/big_buck_bunny_240p_1mb.mp4',
+		        'previewImageUrl': 'https://www.sample-videos.com/img/Sample-jpg-image-50kb.jpg'
 		    }";
             foreach ($events as $event) {
                 
