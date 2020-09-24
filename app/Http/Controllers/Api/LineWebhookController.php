050b283048b1b8568a596e55ce2b2383c2d91372
chien_rong chiu <yoyo70202@gmail.com>
2020-09-24 15:00:17 +0800

    same as above

--- app/Http/Controllers/Api/LineWebhookController.php
+++ app/Http/Controllers/Api/LineWebhookController.php
@@ -91,22 +91,25 @@ class LineWebhookController extends Controller
                 }
                 if ($text == "常見問題") {
                 	$actions = array(
-					  //一般訊息型 action
-					  new MessageTemplateActionBuilder("按鈕1","文字1"),
-					  //網址型 action
-					  new UriTemplateActionBuilder("Google","http://www.google.com"),
-					  //下列兩筆均為互動型action
-					  new PostbackTemplateActionBuilder("下一頁", "page=3"),
-					  new PostbackTemplateActionBuilder("上一頁", "page=1")
+                		"你居住在台灣的哪個縣市?"=>array(
+                			//一般訊息型 action
+							new MessageTemplateActionBuilder("按鈕1","文字1"),
+							//網址型 action
+							new UriTemplateActionBuilder("Google","http://www.google.com"),
+							//下列兩筆均為互動型action
+							new PostbackTemplateActionBuilder("下一頁", "page=3"),
+							new PostbackTemplateActionBuilder("上一頁", "page=1")
+                		)
+						
 					);
 
-					$img_url = "https://www.sample-videos.com/img/Sample-jpg-image-50kb.jpg";
-					$button = new ButtonTemplateBuilder("按鈕文字","說明", $img_url, $actions);
-					$msg 	= new TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
-					$lineBot->replyMessage($replyToken,$msg);// 回復使用者輸入
-
-					// $msg = new QuickReplyMessageBuilder($actions);
+					// $img_url = "https://www.sample-videos.com/img/Sample-jpg-image-50kb.jpg";
+					// $button = new ButtonTemplateBuilder("按鈕文字","說明", $img_url, $actions);
+					// $msg 	= new TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
 					// $lineBot->replyMessage($replyToken,$msg);// 回復使用者輸入
+
+					$msg = new QuickReplyMessageBuilder($actions);
+					$lineBot->replyMessage($replyToken,$msg);// 回復使用者輸入
                 }
                 if ($text == "熱銷必敗") {
                 	$msg = new LocationMessageBuilder("群義房屋", "台中市南屯區文心路一段424號", 24.1503955, 120.646975);
